c7be17f8b64d7f752fe3666cef6292ba0fb96a2e
Shreya Gosavi <shreyacgosavi@gmail.com>
2023-11-04 12:30:46 +0530

    done

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,35 +1,20 @@
-/*TASk 1: NUMBER GUESSING GAME
-1. Generate a random number within a specified range, such as 1 to 100.
-
-2. Prompt the user to enter their guess for the generated number.
-
-3. Compare the user's guess with the generated number and provide feedback on whether the guess
-is correct, too high, or too low.
-
-4. Repeat steps 2 and 3 until the user guesses the correct number.
-
-You can incorporate additional details as follows:
-
-5. Limit the number of attempts the user has to guess the number.
-6. Add the option for multiple rounds, allowing the user to play again.
-7. Display the user's score, which can be based on the number of attempts taken or rounds won.*/
 
 import java.util.Scanner;
 import java.util.Random;
 
-public class Task1 
+public class NumberGuessingGame 
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         Random randomnum = new Random();
-        int soln = sc.nextInt(bound:100)+1;
-        int try = 3;
+        int soln = randomnum.nextInt(100)+1;
+        int trial = 3;
         boolean correct = false;
 
         System.out.println("------------------------WELCOME TO A NUMBER GUESSING GAME!------------------------");
         
-        while(try>0)
+        while(trial>0)
         {
             System.out.println("GUESS A NUMBER BETWEEN 1 to 100...(within 3 trials only)");
             int guessedno = sc.nextInt();
@@ -41,14 +26,14 @@ public class Task1
             else if(guessedno>soln)
             {
                 System.out.println("Oops! you guessed a higher number");
-                System.out.println("Now you only have"+ " " + try-1 + " " + "trials left" );
-                try--;
+                System.out.println("Now you only have"+ " " + (trial-1) + " " + "trials left" );
+                trial--;
             }
             else if(guessedno<soln)
             {
                 System.out.println("Oops! you guessed a lower number");
-                System.out.println("Now you only have"+ " " + try-1 + " " + "trials left" );
-                try--;
+                System.out.println("Now you only have"+ " " + (trial-1) + " " + "trials left" );
+                trial--;
             }
            
         }
